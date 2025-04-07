d038a2ebcf42112b332add1558a07b62b2918014
jonaslinset01 <jonalins@stud.ntnu.no>
2025-04-07 12:00:37 +0200

    first changes on branch

--- MassBalance.py
+++ MassBalance.py
@@ -3,43 +3,51 @@ import numpy as np
 import constants5 as const
 import WtFrac as wf
 
-def massBalance(vars):
-    
-    
-    #Strøm2
-    wc2, wh2, wn2, wo2, \
-    wc3, wh3, \
-    wc4, wh4, \
-    wc8, wh8, \
-    m2, m3, m4, m5, m6, m7, m8, m9 = vars
-    
-    # mass balance equations
-    eq1_MS = const.m1 -m9-m2
-    eq2_MS = m7-m3
-    eq3_MS = const.m1+m3-m2-m4
-    eq4_MS = m4-m5
-    eq5_MS = m5 - m9 - m6
-    eq6_MS = m6-m7
-    eq7_MS = m9/wc8-m8
-    eq7_MC = const.wcapture*const.wc1*const.m1 - m9
-    
-    
-    
-    # mass fraction equations
-    eq1_MF = (const.m1*const.wc1+m3*wc3-m4*wc4)/m2 - wc2
-    eq2_MF = (const.m1/m2)*const.wh1-wh2
-    eq3_MF = (const.m1/m2)*const.wn1-wn2
-    eq4_MF = (const.m1/m2)*const.wo1-wo2
-    eq5_MF = wf.WtFracCO2(const.alpha3)-wc3
-    eq6_MF = 1-const.waMEA-wc3-wh3
-    eq7_MF = wf.WtFracCO2(const.alpha4)-wc4
-    eq8_MF = 1-const.waMEA-wc4-wh4
-    eq9_MF = 1/(1+(0.3*const.Mw[1])/(0.7*const.Mw[0]))-wc8
-    eq10_MF = 1-wc8-wh8
-    
-    return [eq1_MS, eq2_MS, eq3_MS, eq4_MS, eq5_MS, eq6_MS, eq7_MS, eq7_MC,
-            eq1_MF, eq2_MF, eq3_MF, eq4_MF, eq5_MF, eq6_MF, eq7_MF, eq8_MF, eq9_MF, eq10_MF]
-    
-    
 
-print(scipy.optimize(massBalance(np.zeros(18))))
+wc3 = wf.WtFracCO2(const.alpha3) 
+wc4 = wf.WtFracCO2(const.alpha4)
+wm3 = wm4 = const.waMEA
+wh3 = 0.7 - wc3
+wh4 = 0.7 - wc4
+
+def massBalances(vars):
+    m2, m3, m4, m5, m6, m8, m9 = vars
+    
+    eq1 = m2 - (const.m1 - m9)
+    eq2 = m4 - (const.m1 + m3 - m2)  
+    eq3 = m5 - m4  
+    eq4 = m6 - (m5 - m9)
+    eq5 = m8 - (m9/const.wcapture)
+    eq6 = m9 - (const.wcapture * const.wc1 * const.m1)
+    eq7 = ((const.m1 * const.wc1 + m3 * wc3 - m4 * wc4)/m2) - const.xc8  
+    
+    return [eq1, eq2, eq3, eq4, eq5, eq6, eq7]
+
+x0 = np.ones(7)  # initial guess for m2, m3, m4, m5, m6, m8, m9
+
+solution = scipy.optimize.root(massBalances, x0)
+m2, m3, m4, m5, m6, m8, m9 = solution.x
+
+wc2 = ((const.m1 * const.wc1) + (m3 * wc3) - (m4 * wc4)) / m2
+wh2 = (const.m1 / m2) * const.wh1
+wn2 = (const.m1 / m2) * const.wn1
+wo2 = (const.m1 / m2) * const.wo1
+wt2 = wc2 + wh2 + wn2 + wo2
+
+print("\nMassestrømmer [kg/s]:")
+print(f"m2 (gass ut): {m2:.2f}")
+print(f"m3 (MEA+CO2 ut): {m3:.2f}")
+print(f"m4 (MEA inn): {m4:.2f}")
+print(f"m5 (=m4): {m5:.2f}")
+print(f"m6: {m6:.2f}")
+print(f"m8: {m8:.2f}")
+print(f"m9 (fanget CO2): {m9:.2f}")
+
+print("\nMassefraksjoner i strøm 2 (gass ut):")
+print(f"CO2: {wc2:.4f}")
+print(f"H2O: {wh2:.4f}")
+print(f"N2:  {wn2:.4f}")
+print(f"O2:  {wo2:.4f}")
+print(f"Total: {wt2:.4f}")
+
+
--- WtFrac.py
+++ WtFrac.py
@@ -2,7 +2,7 @@ import constants5 as const
 
 
 def WtFracCO2(loading):
-    return (loading*const.Mw[0])/((1+ 0.7/0.3)*const.MwMEA)
+    return (loading*(const.Mw[0]/1000))/((1+ 0.7/0.3)*(const.MwMEA/1000))
     
     
 print(f"w_co2_3 = {WtFracCO2(0.18):.3f}")
