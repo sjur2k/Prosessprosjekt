7d5ca7246bf8c183ca14d90d975072af61c484ac
sjur2k <sjur.groven@gmail.com>
2025-04-08 19:35:13 +0200

    sjekk txt filer for data

--- MassBalance.py
+++ MassBalance.py
@@ -2,11 +2,12 @@ import scipy.optimize
 import numpy as np
 import constants5 as const
 import WtFrac as wf
+import os
 
 
 
 # ------------------------------------------------------------
-use_mass_approximation = False 
+use_mass_approximation = False
 #-------------------------------------------------------------
 
 
@@ -59,7 +60,6 @@ solution = scipy.optimize.root(massBalances, x0)
 m2, m4, m6, m8= solution.x
 m9 = const.wcapture*const.m1*const.wc1
 
-
 wn2 = (const.m1*const.wn1)/m2
 wo2 = (const.m1*const.wo1)/m2
 wc2 = ((1-const.wcapture)*const.m1*const.wc1)/m2
@@ -99,3 +99,37 @@ print(f"  6  |  {m6:.2f}  |  0  |  0  | 0  | 0  | {wm6:.2f}  |  {wh3:.2f}  | {wc
 print(f"  7  |  {m6:.2f}  |  0  |  0  | 0  | 0  | {wm6:.2f}  |  {wh3:.2f}  | {wc6:.2f} ")
 print(f"  8  |   {m8:.2f}  |{wc8:.2f} |{wh8:.2f} | 0  | 0  |   0   |   0    |   0   ")
 print(f"  9  |   {m9:.2f}  |{const.wc9:.2f} |  0  | 0  | 0  |   0   |   0    |   0   ")
+
+
+# Skrive data til fil
+# ------------------------------------------------------------
+should_write_to_file = False
+#------------------------------------------------------------
+
+
+if should_write_to_file:
+    m_vec=[const.m1,m2,m6,m4,m4,m6,m6,m8,m9]
+    w_format = ["wc","wh","wn","wo","wm"]
+    w_matrix = [[const.wc1,const.wh1,const.wn1,const.wo1,0],
+                [wc2, wh2, wn2, wo2, 0],
+                [wc3, wh3, 0, 0, wm3],
+                [wc4, wh4, 0, 0, wm4],
+                [wc4, wh4, 0, 0, wm4],
+                [wc6, wh6, 0, 0, wm6],
+                [wc6, wh6, 0, 0, wm6],
+                [wc8, wh8, 0, 0, 0],
+                [1, 0, 0, 0, 0]]
+
+    if use_mass_approximation:
+        filename = "massbalance_with_approx.txt"
+    else: filename = "massbalance_no_approx.txt"
+        
+    with open(filename,"w") as f:
+        for i in range(len(m_vec)):
+            f.write(f"m{i+1}: {m_vec[i]:.2f}\n")
+            for j in range(len(w_matrix[i])):
+                            if w_matrix[i][j]!=0:
+                                f.write(f"{w_format[j]}{i+1}: {w_matrix[i][j]:.2f}\n")
+            f.write("\n")
+            
+
