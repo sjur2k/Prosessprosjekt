9e76118ca4f3b4714afc686be6af35354b01f9fa
sjur2k <sjur.groven@gmail.com>
2025-04-10 12:03:46 +0200

    .

--- enthalpy2.py
+++ enthalpy2.py
@@ -1,8 +1,11 @@
 import constants5 as const
 import scipy as sp
 import numpy as np
+### Constants for the simulation
+T1,T2,T3,T4,T5,T6 = const.T[0:6]
+T8,T9,T10,T11 = const.T[7:11]
 
-useApprox = False
+useApprox = True
 if useApprox:
     filename = 'sim_data/massbalance_with_approx.txt'
 else:
@@ -21,20 +24,38 @@ with open(filename, 'r') as f:
             key, value = line.split(":")
             w[key.strip()]= round(float(value.split("%")[0].strip())/100.0,2)
 
-#Ren H20
+#Ren H20(g)
 def cp_h(T):
     T = T + 273.15 # K
     return const.Aw + const.Bw*T + const.Cw*T**2
+
+#Snitt for ren H20(g)
+def mean_cp_h(T1,T2):
+    T1 = T1 + 273.15 # K
+    T2 = T2 + 273.15 # K
+    return const.Aw + (const.Bw/2)*(T2**2-T1**2)/(T2-T1) + (const.Cw/3)*((T2**3-T1**3)/(T2-T1))
 
 #Ren MEA
 def cp_m(T):
     T = T + 273.15 # K
     return const.Aa + const.Ba*T + const.Ca*T**2
 
+#Snitt for ren MEA
+def mean_cp_m(T1,T2):
+    T1 = T1 + 273.15 # K
+    T2 = T2 + 273.15 # K
+    return const.Aa + (const.Ba/2)*(T2**2-T1**2)/(T2-T1) + (const.Ca/3)*((T2**3-T1**3)/(T2-T1))
+
 #CO2 i MEA-løsning
 def cp_c(T):
     T = T + 273.15 # K
     return const.Ac + const.Bc*T
+
+#Snitt for CO2 i MEA-løsning
+def mean_cp_c(T1,T2):
+    T1 = T1 + 273.15 # K
+    T2 = T2 + 273.15 # K
+    return const.Ac + (const.Bc/2)*(T2**2-T1**2)/(T2-T1)
 
 #MEA-løsning
 def cp_sol(T , is_rich): #([Celsius], Bool)
@@ -44,6 +65,21 @@ def cp_sol(T , is_rich): #([Celsius], Bool)
         key = "wm3"
     wm = w[key]
     return (1-wm)*cp_h(T)+wm*cp_m(T)+wm*(1-wm)*(const.As + const.Bs*T + const.Cs*wm*(T)**(-1.5859))
+
+#Snitt for MEA-løsning
+def mean_cp_sol(T1,T2, is_rich):
+    if is_rich:
+        key = "wm4"
+    else:
+        key = "wm3"
+    wm = w[key]
+    part1 = (1-wm)*mean_cp_h(T1,T2) # Konverterer til K inni
+    part2 = wm*mean_cp_m(T1,T2) # Konverterer til K inni
+    T1 = T1 + 273.15 # K
+    T2 = T2 + 273.15 # K
+    part3 = wm*(1-wm)*(const.As + (const.Bs/2)*(T2**2-T1**2)/(T2-T1) + ((const.Cs*wm)/(-0.5859))*((T2-273.15)**(-0.5859)-(T1-273.15)**(-0.5859))/(T2-T1))
+    return part1 + part2 + part3
+
 
 #Varmekapasitet kJ/(kg K) for strøm i
 def cp_i(T, i): #([Celsius], int)
@@ -67,31 +103,51 @@ def cp_i(T, i): #([Celsius], int)
 
 #Snitt varmekapasitet mellom T1 og T2 for strøm i
 def mean_cp_i(T1,T2,i): #([Celsius], [Celsius], int)
-    integral = sp.integrate.quad(lambda T: cp_i(T,i), T1, T2)[0]
-    return integral / (T2 - T1)
-
+    if i<1 or i>9:
+        print("Feil i strømnummer")
+        return np.nan
+    if i in [1,2,8,9]:
+        return cp_i(T1,i) # cpg[j] er omtrent lik i temperaturområdet 45C-107C
+    else:
+        cp = 0
+        is_rich = False
+        if i in [3,6,7]:
+            is_rich = True
+        cp += w[f"wc{i}"]*mean_cp_c(T1,T2)
+        cp += w[f"wm{i}"]*mean_cp_sol(T1,T2, is_rich)
+        return cp
+    
 def DeltaT_lm_V1(T7): #([Celsius])
-    DeltaT1 = const.T6 - const.T5
-    DeltaT2 = T7 - const.T4
-    if DeltaT2 == 0:
+    DeltaT1 = T6 - T5
+    DeltaT2 = T7 - T4
+    if DeltaT1==0 or DeltaT2 == 0:
         print("DeltaT2 = 0 !!")
         return np.nan
-    if 1/1.4 < DeltaT1/DeltaT2 < 1.4:
+    if DeltaT1/DeltaT2<=0:
+        print(f"Invalid: DT2 = T7-T4 = {T7}-{T4} = {DeltaT2} <0")
+        return np.nan
+    elif 1/1.4 < DeltaT1/DeltaT2 < 1.4:
         return 0.5 * (DeltaT1 + DeltaT2)
     else:
         return (DeltaT1-DeltaT2) / np.log(DeltaT1/DeltaT2)
 
-""" def V1(vars):
-    T7 = vars[0]
-    m3 = m["m3"]
+def V1(vars):
+    T7, A = vars
+
     m4 = m["m4"]
     m6 = m["m6"]
-    m8 = m["m8"]
-    wc3 = w["wc3"]
-    wc4 = w["wc4"]
-    wc6 = w["wc6"]
-    wc8 = w["wc8"]
+    mean_cp_hot = float(mean_cp_i(T6,T7,6))
+    mean_cp_cold = float(mean_cp_i(T4,T5,4))
+    
+    Q = m4*mean_cp_cold*(T5-T4) #kW
+    deltaT_lm = DeltaT_lm_V1(T7) #K
+    U = const.U/1000 #kW/(m^2 K)
+    
+    eq=[0]*2
+    eq[0] = T7 - (T6-Q/(m6*mean_cp_hot))
+    eq[1] = A  - (Q/(const.U*deltaT_lm))
+    return eq
 
-
-    deltaT_lm = DeltaT_lm_V1(T7)
-return "something" """
+sol = sp.optimize.root(V1, [T6-15, 190]).x
+print(sol)
+print(mean_cp_i(T5,T6,4))
